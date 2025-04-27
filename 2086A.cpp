cfb68b8c78839e7d4731b337530a984116074213
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-04-27 21:48:51 +0330

    2086A

---
+++ 2086A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int num_jars;
+		cin >> num_jars;
+		cout << (4 * num_jars) / 2 << '\n';
+	}
+
+
+
+	return 0;
+}
