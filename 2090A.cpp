0de5ed97cfb554bad28722ca0920ec91248f5042
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-04-30 14:17:42 +0330

    2090A

--- 2090A.cpp
+++ 2090A.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
 
+int main()
+{
     int t;
-    cin >> t;
-    while (t--) {
-        long long x, y, a;
-        cin >> x >> y >> a;
+    cin >>t;
 
-     
-        long long low = 0, high = 2LL * (a + 1);
-        long long ans = high;
+    while(t--)
+    {
+        int x,y,a;
+        cin>>x>>y>>a;
 
-        while (low <= high) {
-            long long n = low + (high - low) / 2;
-            long long b_days = (n + 1) / 2; 
-            long long k_days = n / 2;     
-            long long depth = b_days * x + k_days * y;
-
-            if (depth >= a + 1) { 
-                ans = n;
-                high = n - 1;
-            }
-            else {
-                low = n + 1;
-            }
-        }
-
-        cout << (ans % 2 == 1 ? "NO" : "YES") << '\n';
+        int d = a %(x+y);
+        if(x <= d)
+            cout << "Yes"<<endl;
+        else
+          cout << "NO"<<endl;
     }
+
+
+
 
     return 0;
 }
