e81506661d7feba5b2e20edb9a64f7508aa9946e
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-05-09 16:56:08 +0330

    2094A

---
+++ 2094A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	long t; cin >> t;
+	while (t--)
+	{
+		string a, b, c; cin >> a >> b >> c;
+		cout << a[0] << b[0] << c[0] << endl;
+	}
+
+
+	return 0;
+}
