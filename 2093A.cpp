1059f178622898a4ec6b810952a505b78b282f1c
MohammadNabizada <kingpowerndz1382@gmail.com>
2025-05-14 22:06:23 +0330

    2093A

---
+++ 2093A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+    long t; cin>>t;
+    while(t--){
+        long k; cin>>k;
+        cout<<(k % 2 ? "YES" : "NO");
+    }
+
+}
